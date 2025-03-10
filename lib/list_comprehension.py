2ea40920d32acd80dc13d40ed5e075f483998111
VictoriaCarvalho <vix219@icloud.com>
2025-03-10 13:12:43 -0500

    add lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+print(return_evens)
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+print(make_exclamation([]))  
+
+    
